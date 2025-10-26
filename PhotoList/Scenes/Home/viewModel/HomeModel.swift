a6cc38dc2772751db466798ce4bea709d7e572b8
Pham Khanh Huy <khanhhuypham.1110@gmail.com>
2025-10-26 21:02:09 +0700

    s

--- PhotoList/Scenes/Home/viewModel/HomeModel.swift
+++ PhotoList/Scenes/Home/viewModel/HomeModel.swift
@@ -100,45 +100,7 @@ extension HomeViewModel{
             
         }
     }
-//    @MainActor
-//    func getPhotoList(page: Int) async {
-//        guard !APIParameter.isAPICalling, !APIParameter.isGetFullData else { return }
-//
-//        APIParameter.isAPICalling = true
-//
-//        let result = await service.getPhotoList(limit: APIParameter.limit, page: page)
-//
-//        switch result {
-//            case .success(let data):
-//                guard !data.isEmpty else {
-//                    APIParameter.isGetFullData = true
-//                    APIParameter.isAPICalling = false
-//                    return
-//                }
-//
-//                // update model data
-//                let start = photoList.count
-//                photoList.append(contentsOf: data)
-//
-//                // perform UI updates only if a view is bound
-//                if let tableView = view?.tableView {
-//                    UIView.performWithoutAnimation {
-//                        tableView.performBatchUpdates({
-//                            let end = photoList.count - 1
-//                            let indexPaths = (start...end).map { IndexPath(row: $0, section: 0) }
-//                            tableView.insertRows(at: indexPaths, with: .none)
-//                        }, completion: nil)
-//                    }
-//                }
-//
-//                APIParameter.page = page
-//                APIParameter.isGetFullData = data.count < APIParameter.limit
-//                APIParameter.isAPICalling = false
-//
-//            case .failure:
-//                APIParameter.isAPICalling = false
-//        }
-//    }
+
 
 }
 
