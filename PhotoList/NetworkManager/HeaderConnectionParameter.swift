b027ecb8c7570b5f52a519bbc4396d85ac3353b7
Pham Khanh Huy <khanhhuypham.1110@gmail.com>
2025-10-26 21:01:39 +0700

    remove redundant code

--- PhotoList/NetworkManager/HeaderConnectionParameter.swift
+++ PhotoList/NetworkManager/HeaderConnectionParameter.swift
@@ -7,53 +7,9 @@
 
 
 extension NetworkManager{
-    
-//    @Injected(\.utils) static var utils
-//    
-//    private func headerJava(ProjectId:ProjectID = .PROJECT_ID_ORDER, Method:Method = .GET) -> [String : String]{
-//        
-//        @Injected(\.utils.keyChainUtils) var keyChain
-//        
-//        var projectId:ProjectID = .PROJECT_ID_ORDER_SMALL
-//       
-//        if(ProjectId == .PROJECT_ID_ORDER){
-//            projectId = PermissionUtils.GPBH_1 ? .PROJECT_ID_ORDER_SMALL : .PROJECT_ID_ORDER
-//            
-//        }else{
-//            projectId = ProjectId
-//        }
-//        
-//        var header:[String:String] = [
-//            "Method":String(format: "%d", Method.value),
-//            "ProjectId":String(format: "%d", projectId.value)
-//        ]
-//        
-//        if Constants.login, let token = keyChain.getAccessToken(){
-//            
-//            header["Authorization"] = String(format: "Bearer %@", token)
-//         
-//        }else{
-//             if let config = ManageCacheObject.getConfig(){
-//                 header["Authorization"] = String(format: "Basic %@", config.api_key ?? "")
-//             }
-//        }
-//        
-//        return header
-//    }
-//    
-//    private func headerNode(ProjectId:ProjectID = .PROJECT_ID_ORDER, Method:Method = .GET) -> [String : String]{
-//        @Injected(\.utils.keyChainUtils) var keyChain
-//        
-//        if let token = keyChain.getAccessToken(), ManageCacheObject.isLogin(){
-//            return ["Authorization": String(format: "Bearer %@",token), "ProjectId":String(format: "%d", ProjectId.value), "Method":String(format: "%d", Method.value)]
-//        }else{
-//            return ["Authorization": String(format: "%@", ManageCacheObject.getConfig()?.api_key ?? ""), "ProjectId":String(format: "%d", ProjectId.value), "Method":String(format: "%d", Method.value)]
-//            
-//        }
-//    }
-
+ 
    
-    
+
     var headers: [String : String]? {
         
         switch self{
@@ -61,7 +17,6 @@ extension NetworkManager{
             case .getPhotoList(_,_):
                 return [:]
   
- 
         }
     }
 }
--- PhotoList/NetworkManager/NetWorkManager.swift
+++ PhotoList/NetworkManager/NetWorkManager.swift
@@ -8,7 +8,7 @@
 
 
 import UIKit
-//import AlertToast
+
 
 let networkManager = NetworkManager.self
 
